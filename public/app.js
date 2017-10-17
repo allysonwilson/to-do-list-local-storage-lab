d54b2e7e1a3ba1d1fe84b1bf08ae3ebfc12f31ad
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-10-17 13:22:08 +0100

    Add MVP

--- public/app.js
+++ public/app.js
@@ -7,30 +7,32 @@ var init = function () {
 }
 
 var populate = function (todos) {
-  // this function needs to:
-  // - loop through the array of todos, invoking addItem() for each todo item
+  for(item of todos){
+    this.addItem(item);
+  }
 }
 
 var addItem = function (item) {
   var ul = document.querySelector('#todo-list');
-  // this function needs to:
-  // - create an li element containing the string 'item'
-  // - append the li to the "todo-list" ul
+  var li = document.createElement("li");
+  li.innerText = item;
+  ul.appendChild(li);
 }
 
 var handleButtonClick = function () {
-  // this function needs to:
-  // - get hold of the input box's value
-  // - append it to the "todo-list" ul by invoking addItem()
-  // - add it to local storage by invoking save()
+
+  var inputBox = document.getElementById("new-item");
+  var itemToAppend = inputBox.value
+  addItem(itemToAppend);
+  save(itemToAppend);
 }
 
 var save = function (newItem) {
-  // this function needs to:
-  // - get the data back from local storage and parse to an array
-  // - add the newItem to the array
-  // - stringify the updated array
-  // - save it back to localstoage
+  var jsonString = localStorage.getItem('todoList') || "[]";
+  var savedDataArray = JSON.parse(jsonString);
+  savedDataArray.push(newItem);
+  var stringifiedArray = JSON.stringify(savedDataArray);
+  localStorage.setItem('todoList', stringifiedArray);
 }
 
 window.addEventListener('load', init);
